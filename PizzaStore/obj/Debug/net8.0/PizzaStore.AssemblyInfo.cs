a32c4af068dbe15faa7e7e359034e82c0fbe257d
El Anewar <dmlattanda@gmail.com>
2024-12-04 13:03:25 +0000

    Obtenir un seul élément et Capture d'ecran

--- PizzaStore/obj/Debug/net8.0/PizzaStore.AssemblyInfo.cs
+++ PizzaStore/obj/Debug/net8.0/PizzaStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afbc85a2d25dbf698ef2d9028b2ea91d5f5f2218")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b98d2d61228d0ebc2599b038f1fd7feff84f1042")]
 [assembly: System.Reflection.AssemblyProductAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
