80dabc1d001a7cc79754ad8e79c50d503373a3b7
El Anewar <dmlattanda@gmail.com>
2024-12-04 13:45:08 +0000

    Installation des outils et packages suivants

--- PizzaStore/obj/Debug/net8.0/PizzaStore.AssemblyInfo.cs
+++ PizzaStore/obj/Debug/net8.0/PizzaStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d22f855532b78b768d06ac54bcec8976d41f423a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f640b76a416ab9cbc18f3dbf3e8967c4c191ddf")]
 [assembly: System.Reflection.AssemblyProductAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
