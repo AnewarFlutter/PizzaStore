7296c2c190e7ccb6a47a06e275d3c44f1e022945
El Anewar <dmlattanda@gmail.com>
2024-12-04 12:50:59 +0000

    Tester l’API et capture d'ecran

--- PizzaStore/obj/Debug/net8.0/PizzaStore.AssemblyInfo.cs
+++ PizzaStore/obj/Debug/net8.0/PizzaStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c305cab09e2c0dc184e2cd37a9fbaf4a6e8eaf37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afbc85a2d25dbf698ef2d9028b2ea91d5f5f2218")]
 [assembly: System.Reflection.AssemblyProductAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
